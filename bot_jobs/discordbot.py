6dacff62365d9215abeb30d73f560195be98689a
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2021-07-01 18:31:04 -0400

    Add scheduler

--- bot_jobs/discordbot.py
+++ bot_jobs/discordbot.py
@@ -3,6 +3,7 @@ import os
 
 import discord
 import requests
+from discord.ext import commands, tasks
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -12,6 +13,44 @@ client = discord.Client()
 API_ROOT = "https://api.github.com"
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 # GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
+
+
+class MyCog(commands.Cog):
+    def __init__(self):
+        self.index = 0
+        self.printer.start()
+
+    def cog_unload(self):
+        self.printer.cancel()
+
+    @tasks.loop(seconds=5.0)
+    async def printer(self):
+        print(self.index)
+        self.index += 1
+
+
+@tasks.loop(seconds=86400)
+async def slow_count():
+    # import pdb; pdb.set_trace()
+
+    # for guild in client.guilds:
+    #    discord.utils.get(guild.channels, name='channel', category=category)
+
+    channel = [
+        ob for ob in client.guilds[0].channels if ob.name == 'dev_tests'
+    ][0]
+
+    result = "**Results:**\n"
+    result += "\n".join(
+        [f"- {k}" for k in get_pull_requests("pytorch", "pytorch")]
+    )
+    await channel.send(result)
+    print(f"Task done at {datetime.datetime.now()}")
+
+
+@slow_count.after_loop
+async def after_slow_count():
+    print('task done!')
 
 
 def get_pull_requests(owner: str, repo: str):
@@ -41,6 +80,7 @@ def get_pull_requests(owner: str, repo: str):
 @client.event
 async def on_ready():
     print("We have logged in as {0.user}".format(client))
+    slow_count.start()
 
 
 @client.event
